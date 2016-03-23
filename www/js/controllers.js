80fe718ee0395ad0d8ab80d8f33ff85e8bc5bd66
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2016-03-23 17:52:29 -0500

    showing images in grid

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,21 @@
 angular.module('starter.controllers', [])
 
-.controller('UnicornsCtrl', function($scope) {})
+.controller('UnicornsCtrl', function($scope, $http) {
+  $http.get('http://api.giphy.com/v1/gifs/search?q=unicorn&api_key=dc6zaTOxFJmzC').then(function(resp) {
+    $scope.unicorns = resp.data.data;
+    console.log('SUCCESS', resp.data.data);
+  }, function(err) {
+    console.error('ERR', err);
+  });
+})
 
-.controller('TrendingCtrl', function($scope, Chats) {
+.controller('TrendingCtrl', function($scope, $http) {
+  $http.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC').then(function(resp) {
+    $scope.trends = resp.data.data;
+  }, function(err) {
+    console.error('ERR', err);
+  });
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,18 +24,18 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+  // $scope.chats = Chats.all();
+  // $scope.remove = function(chat) {
+  //   Chats.remove(chat);
+  // };
 });
+
+// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//   $scope.chat = Chats.get($stateParams.chatId);
+// })
+
+// .controller('AccountCtrl', function($scope) {
+//   $scope.settings = {
+//     enableFriends: true
+//   };
+// });

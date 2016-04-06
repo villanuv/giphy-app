0508f0f8fd151ea1c42bfd869b39991721c44831
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2016-04-05 21:59:48 -0500

    added search function, removed search ctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,99 +1,111 @@
 angular.module('starter.controllers', [])
 
-.controller('UnicornsCtrl', function($scope, $ionicModal, UnicornService) {
+.controller('UnicornsCtrl', function($scope, $ionicNavBarDelegate, $ionicModal, $timeout, UnicornService) {
   $scope.limit = 25;
   $scope.offset = 0;
   $scope.unicorns = [];
+  $scope.searchbarData = {terms: 'unicorn'};
 
-  UnicornService.GetUnicorns().then(function(unicorns){
+  console.log($ionicNavBarDelegate.title);
+
+  $scope.update = function(string) {
+    var stringFix = string.terms.replace(/\s+/g, '+');
+    $scope.searchbarData = angular.copy({terms: stringFix});
+    $scope.searchbar = '';
+    $scope.unicorns.length = 0;
+    UnicornService.GetUnicorns($scope.searchbarData.terms).then(function(unicorns){
+      $scope.unicorns = unicorns;
+    });
+    $ionicNavBarDelegate.title('Search: ' + string.terms);
+  };
+
+  UnicornService.GetUnicorns($scope.searchbarData.terms).then(function(unicorns){
     $scope.unicorns = unicorns;
   });
 
   $scope.loadMore = function() {
     $scope.offset += $scope.limit;
 
-    UnicornService.GetNewUnicorns({ limit: $scope.limit, offset: $scope.offset }).then(function(unicorns){
+    UnicornService.GetNewUnicorns({ string: $scope.searchbarData.terms, limit: $scope.limit, offset: $scope.offset }).then(function(unicorns){
       $scope.unicorns = $scope.unicorns.concat(unicorns);
       $scope.$broadcast('scroll.infiniteScrollComplete');
+      $scope.$apply();
     });
   };
 
-  $ionicModal.fromTemplateUrl('modal.html', {
+  $ionicModal.fromTemplateUrl('modal.html', function($ionicModal) {
+    $scope.modal = $ionicModal;
+  }, {
     scope: $scope,
     animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
   });
+
   $scope.openModal = function(image) {
     $scope.modalImg = image;
-    $scope.modal.show();
+    $timeout(function() {
+      $scope.modal.show();
+    }, 700);
   };
+
   $scope.closeModal = function() {
     $scope.modal.hide();
+    $timeout(function() {
+      $scope.modalImg = "/img/spinner.gif";
+    }, 2000);
   };
 
 })
 
-.controller('TrendingCtrl', function($scope, $ionicModal, TrendsService) {
+.controller('TrendingCtrl', function($scope, $ionicNavBarDelegate, $ionicModal, $timeout, TrendsService) {
   $scope.limit = 25;
   $scope.offset = 0;
   $scope.trends = [];
+  $scope.searchbarData = {terms: 'trending'};
 
-  TrendsService.GetTrends().then(function(trends){
+  $scope.update = function(string) {
+    var stringFix = string.terms.replace(/\s+/g, '+');
+    $scope.searchbarData = angular.copy({terms: stringFix});
+    $scope.searchbar = '';
+    $scope.trends.length = 0;
+    TrendsService.GetTrends($scope.searchbarData.terms).then(function(trends){
+      $scope.trends = trends;
+    });
+    $ionicNavBarDelegate.title('Search: ' + string.terms);
+  };
+
+  TrendsService.GetTrends($scope.searchbarData.terms).then(function(trends){
     $scope.trends = trends;
   });
 
   $scope.loadMore = function() {
     $scope.offset += $scope.limit;
 
-    TrendsService.GetNewTrends({ limit: $scope.limit, offset: $scope.offset }).then(function(trends){
+    TrendsService.GetNewTrends({ string: $scope.searchbarData.terms, limit: $scope.limit, offset: $scope.offset }).then(function(trends){
       $scope.trends = $scope.trends.concat(trends);
       $scope.$broadcast('scroll.infiniteScrollComplete');
-    });
-  };
-
-  $ionicModal.fromTemplateUrl('modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  $scope.openModal = function(image) {
-    $scope.modalImg = "";
-    $scope.modalImg = image;
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modalImg = "";
-    $scope.modal.hide();
-  };
-
-})
-
-.controller('SearchCtrl', function($scope, $ionicModal, SearchService) {
-  $scope.searches = [];
-
-  $scope.searchGiphy = function() {
-    SearchService.GetSearch($scope.searchterms).then(function(searches){
-      $scope.searches = searches;
       $scope.$apply();
     });
   };
 
-  $ionicModal.fromTemplateUrl('modal.html', {
+  $ionicModal.fromTemplateUrl('modal.html', function($ionicModal) {
+    $scope.modal = $ionicModal;
+  }, {
     scope: $scope,
     animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
   });
+
   $scope.openModal = function(image) {
-    $scope.modalImg = "";
     $scope.modalImg = image;
-    $scope.modal.show();
+    $timeout(function() {
+      $scope.modal.show();
+    }, 700);
   };
+
   $scope.closeModal = function() {
-    $scope.modalImg = "";
     $scope.modal.hide();
+    $timeout(function() {
+      $scope.modalImg = "/img/spinner.gif";
+    }, 2000);
   };
 
 });

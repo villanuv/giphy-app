46796605be2bf40cb1641b823cc5f64c361db36b
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2016-04-05 22:03:42 -0500

    removed search state

--- www/js/app.js
+++ www/js/app.js
@@ -58,16 +58,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'TrendingCtrl'
       }
     }
-  })
-
-  .state('tab.search', {
-    url: '/search',
-    views: {
-      'tab-search': {
-        templateUrl: 'templates/tab-search.html',
-        controller: 'SearchCtrl'
-      }
-    }
   });
 
   // if none of the above states are matched, use this as the fallback
